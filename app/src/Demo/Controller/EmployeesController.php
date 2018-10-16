c431d5d731ce2a621a82fd5a319caa7efcc4b709
phpDev949 <39319751+phpDev949@users.noreply.github.com>
2018-10-16 14:58:27 -0700

    some how not updated from commandline

--- app/src/Demo/Controller/EmployeesController.php
+++ app/src/Demo/Controller/EmployeesController.php
@@ -28,18 +28,23 @@ class EmployeesController
      */
     public function getEmployees(Request $request, Response $response, array $args = null) :Response
     {
-        ////// get employees table from project ////// 
-	//$sql = $this->container->get('pdo')->prepare("SELECT * FROM employees;");
-	//$sql = $this->container->get('pdo')->prepare("SELECT em.id, first_name, last_name, email, jo.job_title, lo.location_name FROM employees AS em INNER JOIN job_titles AS jo ON em.job_title_id = jo.id INNER JOIN locations AS lo ON em.location_id = lo.id");
-	$sql = $this->container->get('pdo')->prepare("SELECT em.id, first_name, last_name, email, jo.job_title FROM employees AS em INNER JOIN job_titles AS jo ON em.job_title_id = jo.id");
-	$sql->execute();
-	$data = $sql->fetchAll();
-	//print_r($data);
+        ////// get employees table from project //////
+        //$sql = $this->container->get('pdo')->prepare("SELECT * FROM employees;");
+        //$sql = $this->container->get('pdo')->prepare("SELECT em.id, first_name, last_name, email, jo.job_title, lo.location_name FROM employees AS em INNER JOIN job_titles AS jo ON em.job_title_id = jo.id INNER JOIN locations AS lo ON em.location_id = lo.id");
+        $sql = $this->container->get('pdo')->prepare("SELECT em.id, first_name, last_name, email, jo.job_title FROM employees AS em INNER JOIN job_titles AS jo ON em.job_title_id = jo.id");
+        $sql->execute();
+        $em = $sql->fetchAll();
+        $sql_loc = $this->container->get('pdo')->prepare("SELECT * FROM assigned_locations;");
+        $sql_loc->execute();
+        $loc = $sql_loc->fetchAll();
+        //print_r($res_loc);
+        $data = array('em' => $em, 'loc' => $loc);
         //$this->container['logger']->info("Home page action dispatched");
         $this->container->get('logger')->info("Employees Listing page action dispatched");
-	//$this->container['view']->render($response, 'home.twig');
-	$this->container->get('view')->render($response, 'employees.html', array('data' => $data));
-	return $response;
+        //$this->container['view']->render($response, 'home.twig');
+        $this->container->get('view')->render($response, 'employees.html', array('data' => $data));
+        return $response;
     }
 
 }
+~              
